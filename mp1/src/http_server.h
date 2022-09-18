8aaf14b4013cb569f185bc89840bfde2c664249d
HongboZheng <hongboz2@illinois.edu>
2022-09-18 11:56:51 -0500

    :white_check_mark: Finish http_server.c :white_check_mark:

--- mp1/src/http_server.h
+++ mp1/src/http_server.h
@@ -9,14 +9,14 @@
 // function signatures
 void sigchld_handler(int s);
 void *get_in_addr(struct sockaddr *sa);
-int sock_getline(int sock, char *buf, int size);
-int main(int argc, char *argv[]);
-int bind_server(const char *port);
-void handle_client(int client);
-int process_request(char *request, char *method, char *URI, char *version);
-void respond_bad_request(int client);
-void respond_not_found(int client);
-void respond_ok(int client, const char *filename);
+int read_socket(int sock, char *buf, int size);
+void file_not_found(int client);
+void file_found(int client, const char *filename);
+void unknown_request(int client);
 void send_file(int client, const char *filename);
+int bind_server(const char *port);
+int process_request(char *request, char *method, char *URI, char *version);
+void handle_client(int client);
+int main(int argc, char *argv[]);
 
 #endif

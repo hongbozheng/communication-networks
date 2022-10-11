8e740481cc9038d84d41b573c7bcdb0c825f2635
Hongbo Zheng <hongboz2@illinois.edu>
2022-10-10 21:58:14 -0500

    :hammer: Minor fix on receiver_main bash scripts :hammar:

--- mp2/reliable_receiver.bash
+++ mp2/reliable_receiver.bash
@@ -3,15 +3,16 @@ RELIABLE_RECEIVER=reliable_receiver
 PORT=8080
 
 if [ -f "$FILE" ]; then
-    echo "[INFO]: $FILE already exists"
-    echo "[INFO]: Removing $FILE"
+    echo "[INFO]: File $FILE already exists"
+    echo "[INFO]: Removing file $FILE"
     rm $FILE
-    echo "[INFO]: $FILE removed"
-    echo "[INFO]: Creating new txt file $FILE"
+    echo "[INFO]: File $FILE removed"
+    echo "[INFO]: Creating new file $FILE"
     touch $FILE
     echo "[INFO]: $FILE created"
 else
-    echo "[INFO]: Creating txt file $FILE"
+    echo "[INFO]: Missing file $FILE"
+    echo "[INFO]: Creating file $FILE"
     touch $FILE
     echo "[INFO]: $FILE created"
 fi
